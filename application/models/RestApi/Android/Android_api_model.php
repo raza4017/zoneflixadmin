3566e1be752032e0a28d6c5ffeb354db1c5e4db5
Ali Raza <aliraza4017@gmail.com>
2024-09-21 16:21:24 +0500

    android user login limit

--- application/models/RestApi/Android/Android_api_model.php
+++ application/models/RestApi/Android/Android_api_model.php
@@ -18,15 +18,13 @@ class Android_api_model extends CI_Model {
 	function login($email, $password, $device_id=null) {
         $this->db->where('email', $email);  
         $this->db->where('password', $password);  
-        $query = $user = $this->db->get('user_db');  
+        $query = $this->db->get('user_db');  
   
         if ($query->num_rows() == 1) {
             $g = $query->result_array();
-			foreach ($user->result() as $row) {
-                if($row->device_id != $device_id){
-					return false;
-				}
-            }
+			if($g[0]->device_id != $device_id){
+				return false;
+			}
 			$data = array_shift($g);
 			return $data;
         } else {
